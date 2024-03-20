fb04978f562f13d2b20ec8e611862bee207c20db
Brandon White <bjwhite@fnal.gov>
2024-03-20 13:17:41 -0600

    Update supported Rucio versions.

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
-SUPPORTED_VERSION = ["1.29", "1.30", "1.31", "32", "33"]
+SUPPORTED_VERSION = ["1.29", "1.30", "1.31", "32", "33", "34"]
 
 #def get_algorithms():
 #    return { 'lfn2pfn': { 'RUBIN': lfn2pfn_RUBIN }, 'surl': { 'rubin': construct_surl_rubin } }
